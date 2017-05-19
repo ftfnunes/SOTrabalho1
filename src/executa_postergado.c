429ff7d96c821a28a7670de9609a521701c52c6e
Victória Goularte <victoriagr@aluno.unb.br>
2017-05-19 00:49:49 -0300

    Validação dos parâmetros da solicitação de execução.

--- src/executa_postergado.c
+++ src/executa_postergado.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
-int main(){
+int main(int argc, char* args[]) {
+
+
+	// validação dos parâmetros da solicitação de execução    
+    if (argc <= 2 || argc > 3) {
+        printf("\nO primeiro parametro deveria ser fornecido como\n");
+        printf("inteiro representando o delay de execucao.\n");
+        printf("O parametro seguinte deve ser o nome do arquivo \n");
+        printf("executavel a ser executado de maneira postergada.\n");
+        printf("Exemplo:\n");
+        printf("\t> executa_postergado 5 hello world\n\n");
+        return 0;
+    }
+
+	printf("passou\n");    
+
 	return 0;
 }
