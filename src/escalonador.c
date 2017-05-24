fc32d0cf52ddc9d7d6b1002c1ddcef2d4b5044df
Fabio Marques <140039082@aluno.unb.br>
2017-05-23 23:10:05 -0300

    Versão final do escalonador funcional e comentado.

--- src/escalonador.c
+++ src/escalonador.c
@@ -18,18 +18,10 @@
 
 
 /*
-	Mecanismo para escalonar os processos a serem executados.
+	escalonador.c
 
-	Key para obtenção da fila de mensagens: A_DEFINIR .
-
-	É necessário definir a estrutura de dados que irá estar na mensagem recebida pelo gerenciador de solicitação de execução:
-
-	struct solicitacao{
-		long pid;
-		//Estrutura da mensagem
-	}
-
-	Também é necessário definir a estrutura a ser enviada para os gerenciadores de execução
+	Objetivo:
+		Receber solicitações de execução de programas e gerenciá-las. Imprimir informações de execução.
 */
 
 extern int errno;
@@ -45,16 +37,20 @@ int shm_pids, idsem;
 pids_t *pids;
 struct sembuf operacao[2];
 
+/* Estrutura que armazena a mensagem de solicitação recebida. */
 mensagem_sol_t msg_sol;
+
+/* Estrutura que armazena a mensagem a ser enviada para os gerentes de execução com o programa a ser executado. */
 mensagem_exec_t msg_exe;
 
 
+/* Rotina executada ao receber o sinal SIGUSR2, rotina que encerrará a execução dos filhos criados no fork dentro da função main. */
 void trata_sigusr2(){
 	printf("O programa '%s' não será executado devido ao término do processo.\n", msg_sol.info.programa);
 	exit(0);
 }
 
-
+/* Função P do semáforo. */
 void p_sem(){
 	/* Operação que verifica se o semáforo é igual a 0.*/
 	operacao[0].sem_num = 0;
@@ -69,6 +65,8 @@ void p_sem(){
 		printf("Erro na operação P.\n");
 }
 
+
+/* Função V do semáforo. */
 void v_sem(){
 	/* Operação que retorna o semáforo a 0.*/
 	operacao[0].sem_num = 0;
@@ -80,7 +78,7 @@ void v_sem(){
 }
 
 
-
+/* Função que instancia um gerente de execução e retorna o pid do gerente. */
 int instancia_gerente_de_execucao(int num_do_gerente){
 	int pid;
 	char num_gerente_string[3];
@@ -93,6 +91,8 @@ int instancia_gerente_de_execucao(int num_do_gerente){
 	return pid;
 }
 
+
+/* Instancia as filas de mensagem entre os gerentes de execução. */
 int instancia_filas(){
 	int i, id = -1;
 
@@ -106,9 +106,12 @@ int instancia_filas(){
 	return id;
 }
 
+
+/* Essa função remove os recursos utilizados. */
 void remove_recursos() {
 	int i, id;
 
+	/* Remove todas as filas de comunicação entre os processos gerentes de execução. */
 	for(i = 0; i<16; i++){
 		if((id = msgget(FILA_0_K+i, 0666)) < 0){
 			printf("Erro na remocao da fila 0x%x\n", FILA_0_K+i);
@@ -118,26 +121,34 @@ void remove_recursos() {
 			(msgctl(id, IPC_RMID, NULL) == 0) ? printf("Fila 0x%x removida\n", FILA_0_K+i) : printf("Erro, fila 0x%x nao pode ser removida\n", FILA_0_K+i);
 		}
 	}
-
-	/* Envia sinais SIGUSR2 para os filhos do escalonador, indicando a eles que devem terminar imediatamente.*/
-	for (i = 0; i < conta_procs; ++i){
-		kill(pids_procs[i], SIGUSR2);
-	}
 	
-
+	/* Remove a fila de mensagens de solicitação de execução. */
 	(msgctl(filaSolicitacoes, IPC_RMID, NULL) == 0) ? printf("Fila de solicitacoes removida\n") : printf("Erro, fila de solicitacoes nao pode ser removida\n");
+
+	/* Remove a fila de mensagem entre o gerente de execução 0 e o escalonador. */
 	(msgctl(nodesToEsc, IPC_RMID, NULL) == 0) ? printf("Fila nodesToEsc removida\n") : printf("Erro, fila nodesToEsc nao pode ser removida %d\n", errno);
+
+	/* Remove a área de memória compartilhada que armazena os pids do escalonador e do gerente de execução. */
 	(shmctl(shm_pids, IPC_RMID, NULL) == 0) ? printf("Vetor de pids removido\n") : printf("Erro, vetor de pids nao pode ser removido\n");
 
+	/* Remove o semáforo utilizado. */
 	(semctl(idsem, 1, IPC_RMID, NULL) == 0) ?  printf("Semaforo removido\n") : printf("Erro, semaforo nao pode ser removido\n");
 }
 
 
+/* Finaliza o escalonador removendo recursos, realizando wait para os gerentes de execução e para os filhos do escalonador. */
 void finaliza_escalonador(){
 	int i, estado;
+
 	remove_recursos();
+
 	for(i=0; i<16; i++){
 		wait(&estado);
+	}
+
+	/* Envia sinais SIGUSR2 para os filhos do escalonador, indicando a eles que devem terminar imediatamente.*/
+	for (i = 0; i < conta_procs; ++i){
+		kill(pids_procs[i], SIGUSR2);
 	}
 	/* Registrando o wait com o PID do filho antes de continuar o loop. */
 	for(i = 0; i < conta_procs; i++){
@@ -186,7 +197,7 @@ int main(){
 		exit(1);
 	}
 
-	/* Atribuição à variável o ponteiro para a memória compartilhada. */
+	/* Atribuição à variável o ponteiro para a memória compartilhada. A área servirá para armazenar os pids dos gerentes de execução e do escalonador. */
 	pids = (pids_t*) shmat(shm_pids, 0, 0);
 	if (pids < 0) {
 		printf("Erro na associacao da memoria compartilhada\n");
@@ -202,7 +213,7 @@ int main(){
 	printf("Areas de memoria compartilhada (de pids e matriz de ocupacao) e semaforo criados com sucesso!\n");
 
 
-
+	/* Instanciamento dos 16 gerentes de execução e armazenamento de seus pids. */
 	printf("Pids dos gerentes: ");
 	for(i = 0; i < 16; i++){
 		pids->pids_v[i] = instancia_gerente_de_execucao(i);
@@ -212,29 +223,35 @@ int main(){
 
 	printf("\nGerentes de execucao instanciados com sucesso!\n");
 
+	/* Loop infinito do escalonador. */
 	while(1){
-
+		/* Recepção blocante de mensagem de solicitação de execução. */
 		if(msgrcv(filaSolicitacoes, &msg_sol, sizeof(msg_sol), 0, 0) < 0){
 			printf("Erro na recepcao de solicitacao no escalonador\n");
 			exit(1);
 		}
 
+		/* Incremento do número do job ao chegar nova solicitação. */
 		++job;
 
 		/* Atribui à variável 'tempo' o tempo em que a mensagem foi recebida. */
 		time(&tempo);
 
+		/* Pai cria filho que tratará o dealy do programa, o envio do programa para os gerentes de execução e de recepção da mensagem de estatísticas ao final das execuções do programa. */
 		pid = fork();
 
 		if(pid == 0){
 
+			/* Quando o filho recebe o sinal SIGUSR2, desvia o fluxo de execução para o encerramento do processo. */
 			signal(SIGUSR2, trata_sigusr2);
 
 			/* O processo irá realizar um sleep até que que delay desejado seja simulado. */
-			sleep(msg_sol.info.seg);			/* Loop verifica se todos os gerenciadores de processo estão livres.*/
+			sleep(msg_sol.info.seg);			
 
+			/* Operação "P" de semáforo que garante que todos os gerentes de execução executarão somente um programa até que todos fiquem livres novamente. */
 			p_sem();
 
+			/* Serão enviadas 16 mensagens: 1 para cada gerente de execução. */
 			for(i = 15; i >= 0; --i) {
 				/* Atribui 1 ao tipo, pois esse tipo não é relevante nos processos gerenciadores de execução (não são verificados os tipos das mensagens) */
 				msg_exe.mtype = 1;
@@ -250,7 +267,7 @@ int main(){
 			}
 
 
-
+			/* Após o envio de todas as solicitações, o filho espera as mensagens de estatísticas de execução de cada gerente ao final da execução do programa de forma blocante, garantindo que o loop só irá terminar após todos os gerentes finalizarem sua execução. Somente um programa será executado por todos, por vez. */
 			for(i = 0; i < 16; ++i){
 				if(msgrcv(nodesToEsc, &res, sizeof(res), 0, 0) < 0){
 					printf("Erro na recepcao de resposta para o escalonador com erro %d\n", errno);
@@ -260,16 +277,19 @@ int main(){
 				printf("job = %d, arquivo = %s, delay = %d, makespan = %ld\n", job, msg_sol.info.programa, msg_sol.info.seg, res.info.turnaround);
 			}
 
+
+			/* O semáforo é liberado e o processo é encerrado. */
 			v_sem();			
 
 			exit(0);
 		}
 
+		/* O escalonador armazena o pid de seu filho criado e incrementa o número de filhos que possui. */
 		pids_procs[conta_procs] = pid;
 		++conta_procs;
 
 		
-		/* Registrando o wait com o PID do filho antes de continuar o loop. */
+		/* É executado um loop de waitpid não blocantes que removem os filhos já encerrados. */
 		for(i = 0; i < conta_procs; i++){
 			waitpid(pids_procs[i], &estado, WNOHANG);
 		}
